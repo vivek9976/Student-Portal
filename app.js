bd0f0cfc21c4bc4c4622339506dd449a6b203930
vivek jaiswal <vivekjais9919838696@gmail.com>
2023-09-13 22:48:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const loader = require("./loader");
+const app = loader.app;
+
+app.listen(process.env.PORT, (err) =>
+{
+    if(err)console.log("Error while listening : ", err)
+    else console.log(`Listening on port ${process.env.PORT}`)
+});
